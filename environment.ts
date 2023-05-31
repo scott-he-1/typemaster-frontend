dcf15c1d525893bae0e1248e443ee585ee634b42
Scott He <scotthe234@gmail.com>
2023-05-31 14:27:59 -0400

    updated urls

--- environment.ts
+++
@@ -1,3 +0,0 @@
-export const environment = {
-  DATABASE_URL:"typemaster-backend-production.up.railway.app"
-}
--- src/app/high-scores.service.ts
+++ src/app/high-scores.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-import { environment } from 'environment';
+import { environment } from 'src/environments/environment';
 import { map } from 'rxjs/operators';
 import { Score } from './types';
 
